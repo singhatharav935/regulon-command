ec94aa2e9b6aada3210d57191c96f1a8ea76a3c1
Rishabh Shukla <rishabhshukla2510@gmail.com>
2026-05-02 00:10:40 +0530

    Update

--- scripts/test_connection.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-http.get('http://localhost:3001/health', (res) => {
-  let data = '';
-  res.on('data', (chunk) => data += chunk);
-  res.on('end', () => console.log('Backend connected:', data));
-}).on('error', (err) => console.log('Backend not reachable:', err.message));
--- src/pages/UniversityDemoDashboard.tsx
+++
@@ -1,7 +0,0 @@
-import UniversityDashboardShell from "@/components/university/UniversityDashboardShell";
-
-const UniversityDemoDashboard = () => {
-  return <UniversityDashboardShell mode="demo" />;
-};
-
-export default UniversityDemoDashboard;
